c01504316f76d048d9014d9cbebba87dc9e5c3da
Genocide12 <129229824+Genocide12@users.noreply.github.com>
2025-01-19 21:18:53 +0300

    Update final.py
    данные от пользователя теперь хранятся в словаре, а не в списке.

--- final.py
+++ final.py
@@ -1,30 +1,31 @@
-# ВВод данных
+# Ввод данных
 username = input("Введите имя: ")
 content = input("Введите содержание заметки: ")
 status = input("Введите статус заметки: ")
-creation_date = input("Введите дату создания заметки: ")
-modification_date = input("Введите дату изменения заметки: ")
+creation_date = input("Введите дату создания заметки (в формате ГГГГ-ММ-ДД): ")
+modification_date = input("Введите дату изменения заметки (в формате ГГГГ-ММ-ДД): ")
 
 # Ввод заголовков заметки
 title_1 = input("Введите первый заголовок: ")
 title_2 = input("Введите второй заголовок: ")
 
-# Создание списка заголовков
-note = [
-    username,
-    content,
-    status,
-    creation_date,
-    modification_date,
-    [title_1, title_2]
-]
+# Создание словаря для заметки
+note = {
+    "username": username,
+    "content": content,
+    "status": status,
+    "creation_date": creation_date,
+    "modification_date": modification_date,
+    "titles": [title_1, title_2],  # список заголовков
+}
+
 # Вывод данных
 print("\nДанные заметки:")
-print(f"Имя пользователя: {note[0]}")
-print(f"Содержание заметки: {note[1]}")
-print(f"Статус: {note[2]}")
-print(f"Дата создания: {note[3]}")
-print(f"Дата изменения: {note[4]}")
+print(f"Имя пользователя: {note['username']}")
+print(f"Содержание заметки: {note['content']}")
+print(f"Статус: {note['status']}")
+print(f"Дата создания: {note['creation_date']}")
+print(f"Дата изменения: {note['modification_date']}")
 print("Заголовки:")
-print("Заголовок 1: ", note[5][0])
-print("Заголовок 2: ", note[5][1])
+print(f"Заголовок 1: {note['titles'][0]}")
+print(f"Заголовок 2: {note['titles'][1]}")
